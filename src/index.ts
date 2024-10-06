b04ced005d1f04cf14a05c848adbf02311c8ce82
imspace <spacemeowx2@gmail.com>
2024-10-06 17:48:31 +0800

    fix outdated command

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { Context, Logger, Schema } from 'koishi'
 
-import {
-  BundleItem,
-  MapData,
-  duration,
-  new_crafting_data,
-  translate
-} from './type'
-
 export const name = 'apex-helper'
 
 export interface Config {
@@ -24,65 +16,89 @@ export const Config: Schema<Config> = Schema.object({
 
 const apiBaseUrl = 'https://api.mozambiquehe.re'
 
+interface MapInfo {
+  start: number
+  end: number
+  readableDate_start: string
+  readableDate_end: string
+  map: string
+  code: string
+  DurationInSecs: number
+  DurationInMinutes: number
+  isActive?: boolean
+  eventName?: string
+  eventName_zh?: string
+  asset?: string
+  remainingSecs?: number
+  remainingMins?: number
+  remainingTimer?: string
+}
+interface MapItem {
+  current: MapInfo
+  next: MapInfo
+}
+interface MapData {
+  battle_royale: MapItem
+  ranked: MapItem
+  ltm: MapItem
+}
+
+function t(key: string | undefined) {
+  const logger = new Logger('apex-helper')
+
+  if (!key) {
+    logger.warn('Map is undefined')
+    return '未知'
+  }
+
+  const DICT: Record<string, string | undefined> = {
+    'Olympus': '奥林匹斯',
+    'Kings Canyon': '诸王峡谷',
+    'Storm Point': '风暴点',
+    'World\'s Edge': '世界尽头',
+    'Broken Moon': '残月',
+    'E-District': '电力区域',
+    'Control': '控制',
+    'TDM': '团队死斗',
+    'TDM - DeadEye': '团队死斗 - 无护盾',
+    'Gun Run': '子弹时间',
+    'Lockdown': '封锁'
+  }
+  const translated = DICT[key]
+
+  if (!translated) {
+    logger.warn(`Unknown map: ${key}`)
+  }
+
+  return translated || key
+}
+
+function date(d: number) {
+  return new Date(d * 1000).toLocaleTimeString()
+}
+
 export function apply(ctx: Context, config: Config) {
   const logger = new Logger('apex-helper')
 
   logger.debug('Apex Legend helper started')
 
-  ctx.command('apexhelper.map', '查询当前地图轮换').action(async _ => {
-    try {
-      const map = (await ctx.http.get(`${apiBaseUrl}/maprotation`, {
-        params: {
-          auth: config.apikey,
-          version: 2
-        }
-      })) as MapData
-      return (
-        `当前：${translate.event(map.battle_royale.current.map)}\n` +
-        `下个：${translate.event(map.battle_royale.next.map)}\n` +
-        `排位：${translate.event(map.ranked.current.map)}\n` +
-        `轮换：${map.battle_royale.current.remainingMins} 分钟后`
-      )
-    } catch (_) {
-      return '请求失败。'
-    }
-  })
-
-  ctx.command('apexhelper.craft', '查询当前制造轮换').action(async _ => {
-    try {
-      const crafting = new_crafting_data(
-        (await ctx.http.get(`${apiBaseUrl}/crafting`, {
-          params: {
-            auth: config.apikey
-          }
-        })) as BundleItem[]
-      )
-      const gen = (v: BundleItem) => {
-        return v.bundleContent.map(e => {
-          if (v.bundle == 'weapon') {
-            return translate.bundle.weapon(e)
-          }
-          return translate.bundle.common(e)
-        })
+  ctx.command('apexmap').action(async () => {
+    const res = JSON.parse((await ctx.http.get(`${apiBaseUrl}/maprotation`, {
+      params: {
+        auth: config.apikey,
+        version: 2
       }
-      return (
-        `每日：${
-          crafting['daily'] ? gen(crafting['daily']).join(' ') : '未获取'
-        }\n` +
-        `每周：${
-          crafting['weekly'] ? gen(crafting['weekly']).join(' ') : '未获取'
-        }\n` +
-        `武器：${
-          crafting['weapon'] ? gen(crafting['weapon']).join(' ') : '未获取'
-        }\n` +
-        `轮换：<i18n:time value="${
-          crafting['daily'] ? duration(crafting['daily']) : '未获取'
-        }" /> | <i18n:time value="${
-          crafting['weekly'] ? duration(crafting['weekly']) : '未获取'
-        }" />`
-      )
-    } catch (_) {
-      return '请求失败。'
+    })))
+
+    if (res.Error || !res.battle_royale) {
+      logger.error(typeof res)
+      return `查询地图轮换失败：${res.Error || 'Unknown'}`
     }
+    const { battle_royale, ranked, ltm } = res as MapData
+    return [
+      `匹配：${t(battle_royale.current.map)}(${date(battle_royale.current.start)}) -> ${t(battle_royale.next.map)}(${date(battle_royale.next.start)})`,
+      `排位：${t(ranked.current.map)}(${date(ranked.current.start)}) -> ${t(ranked.next.map)}(${date(ranked.next.start)})`,
+      `娱乐：${t(ltm.current.eventName)}(${date(ltm.current.start)}) -> ${t(ltm.next.eventName)}(${date(ltm.next.start)})`
+    ].join('\n')
   })
 }
